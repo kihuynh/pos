255cb6ba83f843d7b43c8ac07a3407b52f81e2cf
Kimberly Huynh and logan tanous <Guest@Epicodus-7E.local>
2018-02-27 16:23:05 -0800

    add comments to app.rb

--- app.rb
+++ app.rb
@@ -9,14 +9,14 @@ require('pry')
 require('pg')
 
 DB = PG.connect({:dbname => 'point_of_sale'})
-
+# displays all products
 get('/') do
   @products = Product.all
   # @customers = Customer.all
   # @purchases = Purchase.all
   erb(:home)
 end
-
+# displays all products in admin page. have ability to delete
 get('/admin') do
   @products = Product.all
   erb(:admin)
@@ -45,15 +45,19 @@ get '/purchase_page/:id' do
   @products = Product.find(params.fetch("id").to_i())
   erb(:purchase_page)
 end
-
+# grab customer info. and info of tables
 post '/purchase_page/:id' do
   customer_name = params.fetch("name")
   customer_age = params.fetch("age").to_i
   purchase_method = params.fetch("payment")
   @customer = Customer.create({:name => customer_name, :age => customer_age})
+  # grabbing id from product.id. or somewhere or url
   @products = Product.find(params.fetch("id").to_i())
+  # creates purchase item that has product and customer_ids
   @one_purchase = Purchase.create({:payment => purchase_method, :product_id => @products.id, :customer_id => @customer.id})
+  # grabs the item where purchase's customer_id = customer's id
   @customer_specific = Customer.where(id: @one_purchase.customer_id).first
+  # similar to above
   @product_specific = Product.where(id: @one_purchase.product_id).first
   erb(:success_page)
 end
